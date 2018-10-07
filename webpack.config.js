f86eb7434d6cfa33c9cd06b3b4e852b6272c51f5
chikuwa111 <take0shun1111@gmail.com>
2018-10-07 15:40:54 +0900

    Update webpack config to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,31 @@
-const path = require('path')
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: ['./src/index.ts'],
   output: {
-    filename: 'bundle.js',
+    filename:
+      process.env.NODE_ENV !== 'production' ? 'bundle.js' : 'bundle-[hash].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/dist/',
   },
   devServer: {
     contentBase: '.',
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      dry: process.env.NODE_ENV !== 'production',
+      exclude: ['.keep'],
+    }),
+    new CopyWebpackPlugin([{ from: 'assets', to: 'assets' }]),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: { minifyCSS: true },
+    }),
+  ],
   module: {
     rules: [
       {
@@ -22,4 +37,4 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.tsx'],
   },
-}
+};
