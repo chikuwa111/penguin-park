bbe3bd20bbf69ed41d711ae21a7af9ed133f68eb
chikuwa111 <take0shun1111@gmail.com>
2018-10-07 12:20:07 +0900

    Make canvas full

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ const app = new PIXI.Application({
 });
 
 // Fit screen size
-// app.renderer.view.style.position = 'absolute'
-// app.renderer.view.style.display = 'block'
-// app.renderer.autoResize = true
-// app.renderer.resize(window.innerWidth, window.innerHeight)
+app.renderer.view.style.position = 'absolute';
+app.renderer.view.style.display = 'block';
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(app.view);
 
