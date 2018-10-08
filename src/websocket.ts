129158d806cc86821a57507f48986ee1a1c208da
chikuwa111 <take0shun1111@gmail.com>
2018-10-08 17:02:02 +0900

    Fix websocket url

--- src/websocket.ts
+++ src/websocket.ts
@@ -2,7 +2,7 @@ import throttle from 'lodash/throttle';
 import Penguin, { PenguinStatus } from './penguin';
 import toast from './toaster';
 
-const WEBSOCKET_SERVER_URL = 'ws://104.196.224.21:3147/ws';
+const WEBSOCKET_SERVER_URL = 'wss://penguin-park-server.chikuwa111.com/ws';
 
 type Message =
   | {
